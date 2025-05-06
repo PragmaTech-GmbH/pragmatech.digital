38fb8b12ed6bc2ee194449236b22d278a1c2ed9f
Philip Riecks <mail@philipriecks.de>
2025-05-06 13:44:42 +0200

    improve logos

--- themes/pragmatech-theme/tailwind.config.js
+++ themes/pragmatech-theme/tailwind.config.js
@@ -1,8 +1,17 @@
-/*
-This file is present to satisfy a requirement of the Tailwind CSS IntelliSense
-extension for Visual Studio Code.
-
-https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
-
-The rest of this file is intentionally empty.
-*/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/**/*.html',
+    './assets/js/**/*.js',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '480px',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
