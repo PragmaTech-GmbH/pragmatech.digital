1619bbeebe1538942dc1587afc8914c7ce226939
Philip Riecks <mail@philipriecks.de>
2025-05-07 11:30:25 +0200

    fonts

--- themes/pragmatech-theme/tailwind.config.js
+++ themes/pragmatech-theme/tailwind.config.js
@@ -5,9 +5,39 @@ module.exports = {
     './assets/js/**/*.js',
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      'serif': ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      'mono': ['Source Code Pro', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+    },
     extend: {
       screens: {
         'xs': '480px',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            fontFamily: ['Inter', 'system-ui', 'sans-serif'].join(','),
+            h1: {
+              fontFamily: ['Inter', 'system-ui', 'sans-serif'].join(','),
+            },
+            h2: {
+              fontFamily: ['Inter', 'system-ui', 'sans-serif'].join(','),
+            },
+            h3: {
+              fontFamily: ['Inter', 'system-ui', 'sans-serif'].join(','),
+            },
+            h4: {
+              fontFamily: ['Inter', 'system-ui', 'sans-serif'].join(','),
+            },
+            code: {
+              fontFamily: ['Source Code Pro', 'monospace'].join(','),
+            },
+            pre: {
+              fontFamily: ['Source Code Pro', 'monospace'].join(','),
+            },
+          },
+        },
       },
     },
   },
